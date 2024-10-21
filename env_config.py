bb972e005114cd277ee16d2bfe09bb4a23abd1b5
Rafael <r_albuq@live.concordia.ca>
2024-10-21 16:52:46 -0400

    key tracking disabled
    During development key was open, now revoking and ignoring local keys

--- env_config.py
+++
@@ -1,3 +0,0 @@
-MAILGUN_API_KEY = '2b706e21e1dd45085a5d98080b39feda-784975b6-fda333ef'
-MAILGUN_DOMAIN = 'sandbox1a12734c4f1348838cf4a95d856af020.mailgun.org'
-DATABASE_URL = "postgresql://flask_store_db_user:xX9u3HafxSz9wA1rj2MZicDSoAzqIukN@dpg-csasec0gph6c73a5mtt0-a.oregon-postgres.render.com/flask_store_db"
