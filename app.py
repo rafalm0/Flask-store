8b9ac1fb549d165ddb4de30cf0a193443aba9524
Rafael <r_albuq@live.concordia.ca>
2024-10-21 15:36:54 -0400

    priority to remote db

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_smorest import abort, Api
 from flask_jwt_extended import JWTManager
 import os
 from flask_migrate import Migrate
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
 from db import db
 import models  # importing models imports the __init__ therefore imports all of them as a package
@@ -20,7 +20,7 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
-    load_dotenv()
+    env_config = dotenv_values()
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
@@ -29,7 +29,7 @@ def create_app(db_url=None):
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or env_config['DATABASE_URL'] or "sqlite:///data.db"
     app.config['SQLALCHEMY_TACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = '261696634396203470738536034261566624783'
 
